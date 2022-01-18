14280a4479e73b09fa16388efc650ecb2ee130a5
NeyamatUllahBulbul <neyamatullahbulbul@gmail.com>
2022-01-18 23:43:19 +0600

    exam routine shown

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
             </div><!-- //LOGO -->
         </div>
         <div class="col-2" style="margin-top: 22px;">
-            <button style="background-color: #23B0ED;border: 1px solid #fff;border-radius:10px; height: 44px;width: 185px; color: white;" href="#"><b>Online Admission</b></button>
+            <button style="background-color: #23B0ED;border: 1px solid #fff;border-radius:10px; height: 44px;width: 185px; color: white;" ><b>Online Admission</b></button>
         </div>
         <div class="col-3">
             <div class="pull-left">
@@ -29,7 +29,7 @@
     <div class="row">
         <div class="col-4"></div>
         <div class="col-4">
-                <form class="search" action="search_result.php" name="search" method="GET">
+                <form class="search" action="search_result_routine.php" name="search" method="GET">
                     <select class="searchTerm" name="batch_id">
                         <option>Select Option</option>
                         <?php foreach ($batches as $batch){ ?>
@@ -43,132 +43,37 @@
         </div>
         <div class="col-4"></div>
     </div>
-    <div class="row text-center mt-4">
+    <div class="row text-center mt-4 mb-3">
         <div class="col-4"></div>
         <div class="col-4">
-            <h1><u>Seat Plans</h1>
+            <h1><a href="index.php"><u>Examination Routine</a></h1>
+            <h1><a href="seat_plans.php"><u>Seat Plans</a></h1>
         </div>
         <div class="col-4"></div>
     </div>
 <?php
-        $sql = "SELECT * FROM seat_plans";
-        $result= $conn->query($sql);
-    foreach ($result as $row){
-        $first_batch    = $row['first_batch'];
-        $first_subject  = $row['first_subject'];
-        $second_batch   = $row['second_batch'];
-        $second_subject = $row['second_subject'];
-        $exam_name      = $row['exam_name'];
-        $date           = $row['date'];
-        $time           = $row['time'];
-        $room_no        = $row['room_no'];
-        $seat           = $row['seat'];
-        $column         = $row['column_number'];
-
-        $sql = "SELECT student_id FROM students WHERE batch_id='$first_batch' AND status='Active'";
-        $first_batch_student= $conn->query($sql);
-        $first_batch_students = [];
-        foreach ($first_batch_student as $first){
-            $first_batch_students[] = $first;
-        }
-        $first_batch_number = $first_batch_student->num_rows;
-
-        $sql = "SELECT student_id FROM students WHERE batch_id='$second_batch' AND status='Active'";
-        $second_batch_student= $conn->query($sql);
-        $second_batch_students = [];
-        foreach ($second_batch_student as $second){
-            $second_batch_students[] = $second;
-        }
-        $second_batch_number = $second_batch_student->num_rows;
-
-        $sql = "SELECT name FROM batches WHERE id='$first_batch'";
-        $first_batch_name= $conn->query($sql);
-
-        $sql = "SELECT name FROM batches WHERE id='$second_batch'";
-        $second_batch_name= $conn->query($sql);
+        $sql = "SELECT er.id,er.photo, batches.name as BName, semesters.name as SName
+                        FROM exam_routine er
+                        INNER JOIN batches ON er.batch_id=batches.id
+                        INNER JOIN semesters ON er.semister_id=semesters.id";
+        $routines= $conn->query($sql);
     ?>
 
     <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-6">
-            <div class="card" id="print">
-                <div class="row mt-5">
-                    <div class="col-lg-12 text-center">
-                        <h2>Stamford University Bangladesh</h2>
-                        <h5><?=$exam_name?></h5>
-                        <h6>Examination date:<?=date('d/m/Y',strtotime($date))?></h6>
-                        <h6>Examination time:<?=date('h:i A',strtotime($time))?></h6>
-                        <h6>Room number:<?=$room_no?></h6>
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <?php foreach ($routines as $routine) { ?>
+                <div class="card">
+                    <div class="card-header">
+                        <h5 style="color: black;">Batch name: <?= $routine['BName'] ?></h5>
+                        <p style="color: black;">Semester name: <?= $routine['SName'] ?></p>
+                        <img style="width: 100%;" src="<?= $routine['photo'] ?>">
                     </div>
                 </div>
-                <div class="row mt-3 mb-3">
-                    <div class="col-1"></div>
-                    <div class="col-5 float-left">
-                        <?php foreach ($first_batch_name as $first_name){ ?>
-                            <h6>Batch name: <?=$first_name['name']?></h6>
-                        <?php } ?>
-                        <h6>Subject: <?=$first_subject?></h6>
-                    </div>
-                    <div class="col-5 text-right">
-                        <?php foreach ($second_batch_name as $second_name){ ?>
-                            <h6>Batch name: <?=$second_name['name']?></h6>
-                        <?php } ?>
-                        <h6>Subject: <?=$second_subject?></h6>
-                    </div>
-                    <div class="col-1"></div>
-                </div>
-                <div class="card-b text-center">
-                    <table id="bootstrap-data-table-export" class="table table-bordered" >
-                        <thead>
-                        <tr>
-                            <?php for ($i=1;$i<=$column;$i++){ ?>
-
-                                <th>Column <?= $i ?></th>
-
-                            <?php } ?>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr >
-                            <?php
-                            $seat_per_column = $seat / $column;
-                            $room = [];
-                            $j = 0;
-                            for ($i=1;$i<=$column;$i++){
-                                echo '<td>';
-                                if ($i%2 == 1) {
-                                    $j++;
-                                    foreach ($first_batch_students as $index=>$first_batch_student) {
-                                        if ($index < ($seat_per_column*$j)) { ?>
-
-                                            <p style="color: black!important;font-size: 25px;"><?=$first_batch_student['student_id']?></p>
-
-                                            <?php        unset($first_batch_students[$index]);
-                                        }
-                                    }
-                                } else {
-                                    foreach ($second_batch_students as $index=>$second_batch_student) {
-                                        if ($index < $seat_per_column*$j) { ?>
-
-                                            <p style="color: black!important;font-size: 25px;"><?=$second_batch_student['student_id']?></p>
-
-
-                                            <?php            unset($second_batch_students[$index]);
-                                        }
-                                    }
-                                }
-                            }
-
-                            ?>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+            <?php } ?>
         </div>
-        <div class="col-md-3"></div>
+        <div class="col-md-2"></div>
     </div>
-<?php } ?>
 <?php include_once 'template/footer.php'?>
 
 <style>
